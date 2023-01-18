be4cf01c2186e130a4339361b56fba362f547477
akeeme <akeemeblack@gmail.com>
2023-01-18 14:53:25 -0500

    textblob instead of spellchecker

--- test.py
+++ test.py
@@ -1,33 +1,30 @@
 import json
-from spellchecker import SpellChecker
+from textblob import TextBlob
 
-spellchecker = SpellChecker()
 
 def check_for_word(string):
-    phishing_words = ['click', 'link', 'account', 'confirm', 'update', 'security', 'bank', 'password', 'login', 'verify', 'secure', 'transaction', 'alert',
+    phishing_words = ['click', 'clicking', 'link', 'account', 'confirm', 'confirming', 'update', 'updating', 'security', 'bank', 'password', 'login', 'verify', 'secure', 'transaction', 'alert',
                       'online', 'bill', 'payment', 'service', 'information', 'personal', 'card', 'activity', 'statement', 'transfer', 'fraud', 'transaction', 'alert', 'fraud']
     count = 0
     for word in phishing_words:
-        if word in string:
+        if word in string.lower():
             count += 1
 
     return count
 
 def check_for_link(string):
-    if 'http' in string:
+    if 'http' in string.lower():
         return True
     else:
         return False
     
 def check_for_misspellings(string):
-    misspellings = spellchecker.unknown(string.split())
-    if len(misspellings) > 0:
-        return True
-    else:
-        return False
+    corrected_text = TextBlob(string).correct()
+
+    return string!=corrected_text
 
 def determine_if_phishing(email):
-    if check_for_word(email['subject']) > 3 or check_for_word(email['body']) > 3:
+    if check_for_word(email['subject']) > 3 or check_for_word(email['body']) > 2:
         if check_for_link(email['body']) == True:
             if check_for_misspellings(email['body']) == True:
                 print(f"Email with subject {email['subject']} is potentially phishing")

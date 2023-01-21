c92c55cb574fe1170e74be5c34983ce8cf581a3b
akeeme <akeemeblack@gmail.com>
2023-01-20 19:47:42 -0500

    added .replit

---
+++ student.py
@@ -0,0 +1,27 @@
+import json
+from textblob import TextBlob
+
+
+def check_for_word(string):
+    phishing_words = ['click', 'clicking', 'link', 'account', 'confirm', 'confirming', 'update', 'updating', 'security', 'bank', 'password', 'login', 'verify', 'secure', 'transaction', 'alert',
+                      'online', 'bill', 'payment', 'service', 'information', 'personal', 'card', 'activity', 'statement', 'transfer', 'fraud', 'transaction', 'alert', 'fraud']
+    pass
+
+
+def check_for_link(string):
+    pass
+
+
+def check_for_misspellings(string):
+    pass
+
+def determine_if_phishing(email):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
